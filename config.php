eca5f5206dfcd255e6607629798e19a1de7217ed
Sel <sofia.sry7@gmail.com>
2025-07-06 07:21:56 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'telegram_token' => '8112615778:AAEgejEi3ssiASCeazLNLcVZedP1pPtiv3c',
+    'telegram_chat_id' => '7937298725',
+    'whatsapp_token' => 'EAAQY3A7KMYsBPDZCr9207McgechS45jV3o5JAbdOZCXbWQaDxro4dIS8ZBETUexIAyiOZAqMXf3oSZBki2nHe1YdT31AJWiQ1IaiG3guCc4MF49vvAEs1tjZAvHs2FpLh5HPcNqxrL5CCO6t9dZArRGHasl1lp0n7LZAJHwLMd38XJnPt9DLfnMhWZCTqZAQcuxZBDkaYleSqzNbTZCGHBtHYCkB08CGyTV3RgOdaBU0uPt6FwZDZD',
+    'whatsapp_phone_id' => '1455961875415510',
+];
